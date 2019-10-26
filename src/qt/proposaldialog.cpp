8e2e7a1a4c4d05511c2dab540ca774a0737c11d5
Manbearpixel <manbearpixel@gmail.com>
2019-10-26 15:21:53 -0500

    UI Cleanups

--- src/qt/proposaldialog.cpp
+++ src/qt/proposaldialog.cpp
@@ -66,8 +66,8 @@ ProposalDialog::ProposalDialog(Mode mode, QWidget* parent) : QDialog(parent), ui
     ui->blockEdit->setValidator(new QIntValidator(1, INT_MAX));
 
     ui->addressEdit->setFont(GUIUtil::bitcoinAddressFont());
-    ui->addressEdit->setPlaceholderText(tr("The Phore public address that will receive the funds"));
-    ui->addressEdit->setToolTip(tr("The Phore public address that will receive the funds"));
+    ui->addressEdit->setPlaceholderText(tr("The public ODIN address that will receive the funds"));
+    ui->addressEdit->setToolTip(tr("The public ODIN address that will receive the funds"));
 
     ui->amountEdit->setFont(GUIUtil::bitcoinAddressFont());
     ui->amountEdit->setPlaceholderText(tr("The amount to be paid per month or cycle"));
@@ -242,7 +242,7 @@ bool ProposalDialog::validateProposal()
     if (nBlockEnd < pindexPrev->nHeight) strError = "Invalid ending block, starting block + (payment_cycle*payments) must be more than current height.";
 
     std::string address = ui->addressEdit->text().toStdString();
-    if (!IsValidDestinationString(address)) strError = "Invalid Phore address";
+    if (!IsValidDestinationString(address)) strError = "Invalid ODIN address";
 
     if (!strError.empty())
     {
